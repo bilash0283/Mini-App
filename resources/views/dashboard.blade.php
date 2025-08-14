b6c57d7726a54814fca7c2e9d3b4d8f4368c72d4
bilash0283 <bilash0283@gmail.com>
2025-08-14 15:15:55 +0600

    dashboard modify

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -83,8 +83,7 @@
                     <svg :class="{'rotate-180': open}" class="w-5 h-5 text-gray-500 transition-transform duration-200" 
                         fill="none" stroke="currentColor" viewBox="0 0 24 24" 
                         xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-                            d="M19 9l-7 7-7-7"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                     </svg>
                 </button>
 
