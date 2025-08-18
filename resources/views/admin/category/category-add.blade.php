bd386ede0a67c00550afd93b9f6f827e83d5ec65
bilash0283 <bilash0283@gmail.com>
2025-08-18 16:14:43 +0600

    category add page new input fild add

--- resources/views/admin/category/category-add.blade.php
+++ resources/views/admin/category/category-add.blade.php
@@ -7,7 +7,7 @@
     </div>
 
     <div class="w-full ">
-        <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+        <form action="#" method="POST" class="mx-auto  max-w-xl">
             <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                 <div class="sm:col-span-2">
                     <label for="first-name" class="block text-sm/6 font-semibold text-black">Category name</label>
@@ -22,6 +22,17 @@
                     <div class="mt-2.5">
                         <textarea id="message" name="description" rows="4"
                             class="block w-full rounded-md bg-black/5 px-3.5 py-2 text-base text-black outline-1 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500"></textarea>
+                    </div>
+                </div>
+
+                <div class="sm:col-span-2">
+                    <label for="message" class="block text-sm/6 font-semibold text-black">Status</label>
+                    <div class="mt-2.5">
+                        <select name="status" class="block w-full rounded-md bg-black/5 px-3.5 py-2 text-base text-black outline-1 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500" id="">
+                            <option >Select One</option>
+                            <option value="1">Active</option>
+                            <option value="2">Inactive</option>
+                        </select>
                     </div>
                 </div>
                 
