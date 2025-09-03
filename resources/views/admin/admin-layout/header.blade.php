45f34bf3d694cb48e088c3ae991dc07e636e32dd
bilash0283 <bilash0283@gmail.com>
2025-09-03 13:27:04 +0600

    blog manage file blog controller

--- resources/views/admin/admin-layout/header.blade.php
+++ resources/views/admin/admin-layout/header.blade.php
@@ -71,7 +71,7 @@
                             class="block px-6 py-2 text-gray-600 hover:bg-gray-100 rounded">
                             Add Blog
                         </a>
-                        <a href=""
+                        <a href="{{ route('blogs.index') }}"
                             class="block px-6 py-2 text-gray-600 hover:bg-gray-100 rounded">
                             Manage Blog
                         </a>
