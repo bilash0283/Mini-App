d535235e46dfa9548dc5d3098b74b1f584009c7b
bilash0283 <bilash0283@gmail.com>
2025-09-03 13:10:04 +0600

    web page modify

--- routes/web.php
+++ routes/web.php
@@ -53,8 +53,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/Category/Delete/{id}', [CategoryController::class, 'delete'])->name('category.delete');
 
     // Blog Manage
-    Route::get('/blogs', [BlogController::class, 'index'])->name('blogs.index');
-    Route::get('/blogs/create', [BlogController::class, 'create'])->name('blogs.create');
+    // Route::get('/blogs', [BlogController::class, 'index'])->name('blogs.index');
+    // Route::get('/blogs/create', [BlogController::class, 'create'])->name('blogs.create');
     // Route::post('/blogs', [BlogController::class, 'store'])->name('blogs.store');
     // Route::get('/blogs/{blog}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
     // Route::put('/blogs/{blog}', [BlogController::class, 'update'])->name('blogs.update');
